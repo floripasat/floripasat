cc0f0a1168dea01bb970b5bbada17cadf21f2868
Frata <msfrata@hotmail.com>
2016-05-25 19:19:39 -0300

    fixing clock time interrupt (beta)

--- obdh/util/sysclock.c
+++ obdh/util/sysclock.c
@@ -15,34 +15,9 @@ uint16_t sysclock_ms = 0;
 
 
 void sysclock_setup(void){
-
-	//Start timer in continuous mode sourced by SMCLK
-	Timer_A_initContinuousModeParam initContParam = {0};
-	initContParam.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
-	initContParam.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
-	initContParam.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_DISABLE;
-	initContParam.timerClear = TIMER_A_DO_CLEAR;
-	initContParam.startTimer = false;
-	Timer_A_initContinuousMode(TIMER_A1_BASE, &initContParam);
-
-	//Initiaze compare mode
-	Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE,
-										 TIMER_A_CAPTURECOMPARE_REGISTER_0
-										 );
-
-	Timer_A_initCompareModeParam initCompParam = {0};
-	initCompParam.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_0;
-	initCompParam.compareInterruptEnable =
-		TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;
-	initCompParam.compareOutputMode = TIMER_A_OUTPUTMODE_OUTBITVALUE;
-	initCompParam.compareValue = SYSCLOCK_INCREMENT;
-	Timer_A_initCompareMode(TIMER_A1_BASE, &initCompParam);
-
-	Timer_A_startCounter(TIMER_A1_BASE,
-						 TIMER_A_CONTINUOUS_MODE
-						 );
-
-
+	  TBCCTL0 = CCIE;                           // TBCCR0 interrupt enabled
+	  TBCCR0 = SYSCLOCK_INCREMENT;
+	  TBCTL = TBSSEL_2 | MC_1 | TBCLR;          // SMCLK, upmode, clear TBR
 }
 
 
@@ -61,12 +36,13 @@ char* sysclock_read(char* strbuff){
 //
 //******************************************************************************
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=TIMER1_A0_VECTOR
-__interrupt
+#pragma vector=TIMERB0_VECTOR
+__interrupt void TIMERB0_ISR (void)
 #elif defined(__GNUC__)
-__attribute__((interrupt(TIMER1_A0_VECTOR)))
+void __attribute__ ((interrupt(TIMERB0_VECTOR))) TIMERB0_ISR (void)
+#else
+#error Compiler not supported!
 #endif
-void TIMER1_A0_ISR(void)
 {
 	sysclock_ms++;
 
