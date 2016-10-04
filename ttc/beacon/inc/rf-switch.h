a87e6b2e898f123824e982d7f886458a2756760b
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2016-10-04 19:32:06 -0300

    fix rf-switch debug mode

--- ttc/beacon/inc/rf-switch.h
+++ ttc/beacon/inc/rf-switch.h
@@ -61,6 +61,10 @@
 #ifndef RF_SWITCH_H_
 #define RF_SWITCH_H_
 
+#ifndef DEBUG_MODE
+#define DEBUG_MODE true
+#endif // DEBUG_MODE
+
 // P5.5 = CTRL_RF_SWT_BEACON
 #define RF_SWT_CONTROL_PORT     GPIO_PORT_P5
 #define RF_SWT_CONTROL_PIN      GPIO_PIN5
